525862be27b7f234a0bc1f37b385e0ee744acd56
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-05-22 16:29:39 +1000

    Attempt to fix remove event

--- g_calendar.py
+++ g_calendar.py
@@ -28,15 +28,16 @@ class CalendarUtils:
 
     #Variable service is not defined. 
     #This is a source of error.
-    def removeCalendarEvent(self, eventID):
+    def removeCalendarEvent(self, ISBN, username):
         """
         A function created to remove a specific google calendar event
 
         Args:
             eventid: the string that identifies the specific event
         """
+        id = ISBN + username
         event = self.service.events().delete(
-            calendarId="primary", eventId=eventID).execute()
+            calendarId="primary", eventId=id).execute()
 
     def createCalendarEvent(self, duedate, ISBN, username):
         """
@@ -56,6 +57,7 @@ class CalendarUtils:
         time_end = "{}T10:00:00+10:00".format(date)
         event = {
             "summary": "ISBN: {}".format(ISBN),
+			"id": ISBN+username,
             "location": "RMIT Building 14",
             "description": "borrowed by:{}, due date:{}".format(
                 username, duedate),
